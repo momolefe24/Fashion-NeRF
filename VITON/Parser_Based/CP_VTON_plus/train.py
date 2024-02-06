9b3680fa6ddfa65be952f736701760b305758387
momolefe24 <momolefe24@gmail.com>
2024-02-06 03:29:32 +0200

    Added changes for TOM

--- VITON/Parser_Based/CP_VTON_plus/train.py
+++ VITON/Parser_Based/CP_VTON_plus/train.py
@@ -319,6 +319,8 @@ def train_tom(opt, train_loader, validation_loader,  gmm_model, model, board, wa
         im = inputs['image'].cuda()
         im_pose = inputs['pose_image']
         im_h = inputs['head']
+        im_c =  inputs['parse_cloth'].cuda()
+        im_cm = inputs['parse_cloth_mask'].cuda()
         shape = inputs['shape']
 
         agnostic = inputs['agnostic'].cuda()
@@ -357,57 +359,29 @@ def train_tom(opt, train_loader, validation_loader,  gmm_model, model, board, wa
         optimizer.step()
 
         if (step+1) % opt.display_count == 0:
-            board_add_images(board, 'combine', visuals, step+1)
-            board.add_image('Real Image', im[0], 0)
-            board.add_image('Head Image', im_h[0], 0)
-            board.add_image('Cloth', c[0], 0)
-            board.add_image('Cloth Mask', (cm*2-1)[0], 0)
-            board.add_image('Warped Cloth', warped_cloth[0], 0)
-            board.add_image('Warped Cloth Mask', (warped_mask*2-1)[0], 0)
-            board.add_image('Composite Mask', (m_composite*2-1)[0], 0)
-            board.add_image('Rendered Image', p_rendered[0], 0)
-            board.add_image('Try-On', p_tryon[0], 0)
-            board.add_scalar('composition_loss', loss.item(), step+1)
-            board.add_scalar('l1_composition_loss', loss_l1.item(), step+1)
-            board.add_scalar('vgg_composition_loss', loss_vgg.item(), step+1)
-            board.add_scalar('mask_composition_loss', loss_mask.item(), step+1)
-            if wandb is not None:
-                my_table = wandb.Table(columns=['Image','Head Image','Cloth','Cloth Mask','Warped Cloth','Warped Cloth Mask','Composite Mask','Rendered Image','Try-On'])
-                real_image_wandb = get_wandb_image(im[0], wandb=wandb)
-                head_image_wandb = get_wandb_image(im_h[0], wandb=wandb)
-                cloth_image_wandb = get_wandb_image(c[0], wandb=wandb)
-                cm_2 = cm*2-1
-                cloth_mask_image_wandb = get_wandb_image(cm_2[0], wandb=wandb)
-                
-                warped_cloth_image_wandb = get_wandb_image(warped_cloth[0], wandb=wandb)
-                warped_cm_2 = warped_mask*2-1
-                warped_cloth_mask_image_wandb = get_wandb_image(warped_cm_2[0], wandb=wandb)
-                
-                m_2 = m_composite*2-1
-                m_composite_image_wandb = get_wandb_image(m_2[0], wandb=wandb)
-                rendered_image_wandb = get_wandb_image(p_rendered[0], wandb=wandb)
-                try_on_wandb = get_wandb_image(p_tryon[0], wandb=wandb)
-                my_table.add_data(real_image_wandb,head_image_wandb,cloth_image_wandb,cloth_mask_image_wandb,
-                               warped_cloth_image_wandb   ,warped_cloth_mask_image_wandb,
-                                  m_composite_image_wandb,rendered_image_wandb,try_on_wandb)
-                wandb.log({'l1_composition_loss': loss_l1.item(),
-                           'vgg_composition_loss':loss_vgg.item(),
-                           'mask_composition_loss':loss_mask.item(),
-                           'composition_loss':loss.item(),
-                'Table':my_table })
+            log_images = {'Image', im[0].cpu().detach() / 2 + 0.5,
+              'Pose Image', im_pose[0].cpu().detach() / 2 + 0.5,
+              'Parse Clothing', im_c[0].cpu().detach() / 2 + 0.5,
+              'Warped Cloth', warped_cloth[0].cpu().detach() / 2 + 0.5,
+              'Warped Cloth Mask', (warped_mask*2-1)[0],
+              'Rendered Image', p_rendered[0].cpu().detach() / 2 + 0.5,
+              'Composition', p_tryon[0].cpu().detach() / 2 + 0.5}
+            log_losses = {'composition_loss', loss.item(),'l1_composition_loss', loss_l1.item(),
+            'vgg_composition_loss', loss_vgg.item(),'mask_composition_loss', loss_mask.item()}
+            log_results(log_images,log_losses, board,wandb, step,iter_start_time=iter_start_time,train=True)
             t = time.time() - iter_start_time
             print('step: %8d, time: %.3f, loss: %.4f, l1: %.4f, vgg: %.4f, mask: %.4f'
                   % (step+1, t, loss.item(), loss_l1.item(),
                      loss_vgg.item(), loss_mask.item()), flush=True)
 
         if (step + 1) % opt.val_count == 0:
-            validate_tom(validation_loader, model,gmm_model, board, wandb=wandb)
+            validate_tom(validation_loader, model,gmm_model, board, step, wandb=wandb)
             model.train()
         if (step+1) % opt.save_period == 0:
             print_log(os.path.join(opt.results_dir, 'log.txt'), f'Save pretrained model to {opt.tom_save_step_checkpoint}')
             save_checkpoint(model, opt.tom_save_step_checkpoint % (step+1))
             
-def validate_tom(validation_loader,model,gmm_model,board, wandb=wandb):
+def validate_tom(validation_loader,model,gmm_model,board, step, wandb=wandb):
     model.cuda()
     model.eval()
 
@@ -417,80 +391,68 @@ def validate_tom(validation_loader,model,gmm_model,board, wandb=wandb):
     criterionL1 = nn.L1Loss()
     criterionVGG = VGGLoss()
     criterionMask = nn.L1Loss()
+    total_batches = len(validation_loader.dataset) // opt.viton_batch_size
+    processed_batches = 0
+    val_composition_loss = 0
+    val_l1_composition_loss = 0
+    val_vgg_composition_loss = 0
+    val_mask_composition_loss = 0
+    with torch.no_grad():
+        while processed_batches < total_batches:
+            inputs = validation_loader.next_batch()
 
-    for step in range(opt.niter + opt.niter_decay):
-        inputs = validation_loader.next_batch()
+            im = inputs['image'].cuda()
+            im_pose = inputs['pose_image']
+            im_h = inputs['head']
+            shape = inputs['shape']
+            im_c =  inputs['parse_cloth'].cuda()
+            im_cm =  inputs['parse_cloth_mask'].cuda()
+            agnostic = inputs['agnostic'].cuda()
+            c = inputs['cloth'].cuda()
+            cm = inputs['cloth_mask'].cuda()
+            pcm = inputs['parse_cloth_mask'].cuda()
 
-        im = inputs['image'].cuda()
-        im_pose = inputs['pose_image']
-        im_h = inputs['head']
-        shape = inputs['shape']
-
-        agnostic = inputs['agnostic'].cuda()
-        c = inputs['cloth'].cuda()
-        cm = inputs['cloth_mask'].cuda()
-        pcm = inputs['parse_cloth_mask'].cuda()
-
-        grid, theta = gmm_model(agnostic, cm)
-        warped_cloth = F.grid_sample(c, grid, padding_mode='border')
-        warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
-        
-        # outputs = model(torch.cat([agnostic, c], 1))  # CP-VTON
-        outputs = model(torch.cat([agnostic, warped_cloth, warped_mask], 1))  # CP-VTON+
-        p_rendered, m_composite = torch.split(outputs, 3, 1)
-        p_rendered = F.tanh(p_rendered)
-        m_composite = F.sigmoid(m_composite)
-        p_tryon = warped_cloth * m_composite + p_rendered * (1 - m_composite)
-
-        """visuals = [[im_h, shape, im_pose],
-                   [c, cm*2-1, m_composite*2-1],
-                   [p_rendered, p_tryon, im]]"""  # CP-VTON
-
-        visuals = [[im_h, shape, im_pose],
-                   [warped_cloth, pcm*2-1, m_composite*2-1],
-                   [p_rendered, p_tryon, im]]  # CP-VTON+
-
-        loss_l1 = criterionL1(p_tryon, im)
-        loss_vgg = criterionVGG(p_tryon, im)
-        # loss_mask = criterionMask(m_composite, cm)  # CP-VTON
-        loss_mask = criterionMask(m_composite, pcm)  # CP-VTON+
-        loss = loss_l1 + loss_vgg + loss_mask
-        board.add_scalar('Val/composition_loss', loss.item(), step+1)
-        board.add_scalar('Val/l1_composition_loss', loss_l1.item(), step+1)
-        board.add_scalar('Val/vgg_composition_loss', loss_vgg.item(), step+1)
-        board.add_scalar('Val/mask_composition_loss', loss_mask.item(), step+1)
-        board.add_image('Val/Real Image', im[0], 0)
-        board.add_image('Val/Head Image', im_h[0], 0)
-        board.add_image('Val/Cloth', c[0], 0)
-        board.add_image('Val/Cloth Mask', (cm*2-1)[0], 0)
-        board.add_image('Val/Warped Cloth', warped_cloth[0], 0)
-        board.add_image('Val/Warped Cloth Mask', (warped_mask*2-1)[0], 0)
-        board.add_image('Val/Composite Mask', (m_composite*2-1)[0], 0)
-        board.add_image('Val/Rendered Image', p_rendered[0], 0)
-        if wandb is not None:
-            my_table = wandb.Table(columns=['Image','Head Image','Cloth','Cloth Mask','Warped Cloth','Warped Cloth Mask','Composite Mask','Rendered Image','Try-On'])
-            real_image_wandb = get_wandb_image(im[0], wandb=wandb)
-            head_image_wandb = get_wandb_image(im_h[0], wandb=wandb)
-            cloth_image_wandb = get_wandb_image(c[0], wandb=wandb)
-            cm_2 = cm*2-1
-            cloth_mask_image_wandb = get_wandb_image(cm_2[0], wandb=wandb)
+            grid, theta = gmm_model(agnostic, cm)
+            warped_cloth = F.grid_sample(c, grid, padding_mode='border')
+            warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
             
-            warped_cloth_image_wandb = get_wandb_image(warped_cloth[0], wandb=wandb)
-            warped_cm_2 = warped_mask*2-1
-            warped_cloth_mask_image_wandb = get_wandb_image(warped_cm_2[0], wandb=wandb)
-            
-            m_2 = m_composite*2-1
-            m_composite_image_wandb = get_wandb_image(m_2[0], wandb=wandb)
-            rendered_image_wandb = get_wandb_image(p_rendered[0], wandb=wandb)
-            try_on_wandb = get_wandb_image(p_tryon[0], wandb=wandb)
-            my_table.add_data(real_image_wandb,head_image_wandb,cloth_image_wandb,cloth_mask_image_wandb,
-                            warped_cloth_image_wandb   ,warped_cloth_mask_image_wandb,
-                                m_composite_image_wandb,rendered_image_wandb,try_on_wandb)
-            wandb.log({'val_l1_composition_loss': loss_l1.item(),
-                        'val_vgg_composition_loss':loss_vgg.item(),
-                        'val_mask_composition_loss':loss_mask.item(),
-                        'val_composition_loss':loss.item(),
-            'Val_Table':my_table })
+            # outputs = model(torch.cat([agnostic, c], 1))  # CP-VTON
+            outputs = model(torch.cat([agnostic, warped_cloth, warped_mask], 1))  # CP-VTON+
+            p_rendered, m_composite = torch.split(outputs, 3, 1)
+            p_rendered = F.tanh(p_rendered)
+            m_composite = F.sigmoid(m_composite)
+            p_tryon = warped_cloth * m_composite + p_rendered * (1 - m_composite)
+
+            """visuals = [[im_h, shape, im_pose],
+                    [c, cm*2-1, m_composite*2-1],
+                    [p_rendered, p_tryon, im]]"""  # CP-VTON
+
+            visuals = [[im_h, shape, im_pose],
+                    [warped_cloth, pcm*2-1, m_composite*2-1],
+                    [p_rendered, p_tryon, im]]  # CP-VTON+
+
+            loss_l1 = criterionL1(p_tryon, im)
+            loss_vgg = criterionVGG(p_tryon, im)
+            # loss_mask = criterionMask(m_composite, cm)  # CP-VTON
+            loss_mask = criterionMask(m_composite, pcm)  # CP-VTON+
+            loss = loss_l1 + loss_vgg + loss_mask
+            loss = loss_l1 + loss_vgg + loss_mask
+            val_composition_loss += loss
+            val_l1_composition_loss += loss_l1
+            val_vgg_composition_loss += loss_vgg
+            val_mask_composition_loss += loss_mask
+            if opt.clip_warping:
+                warped_cloth = warped_cloth * warped_mask + torch.ones_like(warped_cloth) * (1 - warped_mask)
+        log_images = {'Val/Image', im[0].cpu().detach() / 2 + 0.5,
+            'Val/Pose Image', im_pose[0].cpu().detach() / 2 + 0.5,
+            'Val/Parse Clothing', im_c[0].cpu().detach() / 2 + 0.5,
+            'Val/Warped Cloth', warped_cloth[0].cpu().detach() / 2 + 0.5,
+            'Val/Warped Cloth Mask', (warped_mask*2-1)[0],
+            'Val/Rendered Image', p_rendered[0].cpu().detach() / 2 + 0.5,
+            'Val/Composition', p_tryon[0].cpu().detach() / 2 + 0.5}
+        log_losses = {'val_composition_loss', val_composition_loss / len(validation_loader.dataset),'val_l1_composition_loss', val_l1_composition_loss / len(validation_loader.dataset),
+        'val_vgg_composition_loss', val_vgg_composition_loss / len(validation_loader.dataset),'val_mask_composition_loss', val_mask_composition_loss / len(validation_loader.dataset)}
+        log_results(log_images,log_losses, board,wandb, step,train=False)
     
     
 
